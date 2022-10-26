bb2f05e276bdabb96bb24f200df3cbd5232efe5e
bonedaddy <boned4ddy@protonmail.com>
2022-10-26 05:48:42 -0700

    fix init tick array

--- src/instructions/initialize_tick_array.rs
+++ src/instructions/initialize_tick_array.rs
@@ -1,3 +1,4 @@
+
 use anchor_lang::prelude::*;
 
 use crate::state::*;
@@ -10,8 +11,18 @@ pub struct InitializeTickArray<'info> {
     #[account(mut)]
     pub funder: Signer<'info>,
 
-    #[account(mut)]
+    #[account(
+      mut,
+      // seeds = [b"tick_array", whirlpool.key().as_ref(), start_tick_index.to_string().as_bytes()],
+      //bump,
+      //space = TickArray::LEN)]
+    )]
     pub tick_array: AccountLoader<'info, TickArray>,
 
     pub system_program: Program<'info, System>,
+}
+
+pub fn handler(ctx: Context<InitializeTickArray>, start_tick_index: i32) -> Result<()> {
+    let mut tick_array = ctx.accounts.tick_array.load_init()?;
+    Ok(tick_array.initialize(&ctx.accounts.whirlpool, start_tick_index)?)
 }
